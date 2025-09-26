427678a2250d19a092a15e49cd358b8668f581b4
Natriy <dlyutrov08@gmail.com>
2025-09-27 01:27:07 +0300

    Changed C# lr1 code

--- labs_C#/LR1/Program.cs
+++ labs_C#/LR1/Program.cs
@@ -48,22 +48,70 @@
                         --i;
                         continue;
                     }
+                    if (i == 0 && CI[0] == 0)
+                    {
+                        error("Первый коэффициент не может быть нулём.");
+                        --i;
+                        continue;
+                    }
                 }
             }
             
             double D = CI[1] * CI[1] - 4 * CI[2] * CI[0];
-            Console.ForegroundColor = ConsoleColor.Green;
             if (D > 0)
             {
                 double R1 = (-1 * CI[1] + Math.Sqrt(D)) / 2 / CI[0];
                 double R2 = (-1 * CI[1] - Math.Sqrt(D)) / 2 / CI[0];
-                Console.WriteLine("D = {0} > 0\nКорни: {1} и {2}", D, R1, R2);
+                if (R1 < 0 && R2 < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Дискриминант больше нуля. Корней нет.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Environment.Exit(0);
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Дискриминант больше нуля. Корни: ");
+                if (R1 == 0)
+                {
+                    Console.Write("{0} и ", R1);
+                }
+                if (R1 > 0)
+                {
+                    Console.Write("{0}, {1} и ", Math.Sqrt(R1), -1 * Math.Sqrt(R1));
+                }
+                if (R2 == 0)
+                {
+                    Console.Write("{0}", R2);
+                }
+                if (R2 > 0)
+                {
+                    Console.Write("{0}, {1}", Math.Sqrt(R2), -1 * Math.Sqrt(R2));
+                }
+                Console.WriteLine();
             }
-            else if (D == 0) Console.WriteLine("D = 0\nКорень: {0}", -1 * CI[1] / 2 / CI[0]);
+            else if (D == 0)
+            {
+                double R = -1 * CI[1] / 2 / CI[0];
+                if (R == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Дискриминант равен нулю. Единственный корень: {0}", R);
+                }
+                else if (R > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Дискриминант равен нулю. Корни: {0}, {1}", Math.Sqrt(R), -1 * Math.Sqrt(R));
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Дискриминант равен нулю. Корней нет.");
+                }
+            }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("D = {0} < 0", D);
+                Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
             }
 
             Console.ResetColor();
